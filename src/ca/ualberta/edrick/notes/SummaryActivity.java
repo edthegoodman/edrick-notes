12150939820914e65550159d0cc817cc6384e791
gitforschool <edrick@ualberta.ca>
2014-09-26 19:10:18 -0600

    added license

--- src/ca/ualberta/edrick/notes/SummaryActivity.java
+++ src/ca/ualberta/edrick/notes/SummaryActivity.java
@@ -1,3 +1,21 @@
+/*
+edrick-notes : A simple checklist app that can take todo items, check 
+			   them off, archive, unarchive, delete, and email.
+
+Copyright (C) 2014 Edrick de Guzman edrick@ualberta.ca
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
 package ca.ualberta.edrick.notes;
 
 import android.app.Activity;
